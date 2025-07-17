28d098aeb67a9caa80355e051c66e337b4de6eae
MrChrisFabian <christopher.mendoza@fiuni.edu.py>
2025-07-17 15:59:42 -0300

    feat: octokit and .env added

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
+/**
+ * Worker de apoyo para cms de ideal.fiuni.edu.py
+ * Crea una branch, un commit y luego genera la pull request
+ * Para mantener el proceso de control manual en el contenido de la plataforma
+ */
+import { Octokit } from "octokit";
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		return new Response('Hello World! Chris!');
 	},
 } satisfies ExportedHandler<Env>;
